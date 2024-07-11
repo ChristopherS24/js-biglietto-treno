727f93f8f44382266f3149270b123f1ceb38d352
ChristopherS24 <chryskies@gmail.com>
2024-07-11 18:09:28 +0200

    info completate

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
+
+
+
 const km = prompt('Quanti km percorrerai?');
-console.log('Km da percorrere:', km)
+console.log('Km da percorrere:', km);
 
 const age = prompt('Quanti anni hai?');
-console.log('Anni:', age)
+console.log('Anni:', age);
+
+const prezzoalkm = 0.21;
+
+prezzobiglietto=km*prezzoalkm
+console.log('prezzo intero:', prezzobiglietto)
+
+
+//prova
+if (age < 18) {
+    prezzoscontatounder=prezzobiglietto * 0.8;
+    console.log ('Prezzo Scontato Under 18:', prezzoscontatounder);
+}
+else if (age >= 65) {
+    prezzoscontatoover=prezzobiglietto * 0.6;
+    console.log('Prezzo Scontato Over 65:', prezzoscontatoover);
+}
